4a944c7a1ac2ff6fa5a45781877874272ebe5960
Oradra <ogyarjndra@gmail.com>
2025-06-19 14:05:07 +0700

    revisi

--- index.js
+++ index.js
@@ -8,20 +8,42 @@ import registerRoutes from './routes/Register.js';
 import barangRoutes from './routes/Barang.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname  = dirname(__filename);
-const app        = express();
-const port       = process.env.PORT || 3000;
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
 
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json());  
+app.use(express.urlencoded({ extended: false })); 
 
-app.use(cors({
-  origin: 'https://ketemuin.vercel.app',
-  methods: 'GET,POST,PUT,DELETE',
+// app.use(cors({
+//   origin: 'http://localhost:5173', 
+//   methods: 'GET,POST,PUT,DELETE',
+//   credentials: true
+// }));
+
+//sementara
+const corsOptions = {
+  origin: '*',  
+  methods: 'GET, POST, PUT, DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
   credentials: true
-}));
+};
+app.use(cors(corsOptions));
 
+
+// const allowedOrigins = ['https://nusaira.vercel.app', 'http://localhost:5173'];
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static('uploads'));
 
@@ -31,5 +53,5 @@ app.use('/api/barang', barangRoutes);
 
 
 app.listen(port, () => {
-  console.log(`Server berjalan di https://perpus-be.vercel.app/`);
+  console.log(`Server berjalan di http://localhost:${port}`);
 });
